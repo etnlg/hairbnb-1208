9b74d4eae848e282dfc80d8f4be862aba77d220f
Etienne Leguay <etienneleguay@icloud.com>
2023-05-31 11:20:19 -0400

    owner thing

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,22 +1,26 @@
 class BookingsController < ApplicationController
 
-  def index
+  def owner
     @wig_of_the_tenant_ids = Wig.where(user_id: current_user.id).ids
     @bookings = Booking.where(wig_id: @wig_of_the_tenant_ids)
+  end
+
+  def client
+    wigs = Wig.where()
   end
 
   def accept
     # @booking = Booking.find(params[:id])
     Booking.update(params[:id], :satuts => 'accepted')
     # @booking.satuts = "accepted"
-    redirect_to bookings_path
+    redirect_to booking_owner_path
   end
 
   def decline
     # @booking = Booking.find(params[:id])
     Booking.update(params[:id], :satuts => 'declined')
     # @booking.satuts = "accepted"
-    redirect_to bookings_path
+    redirect_to booking_owner_path
   end
 
 end
